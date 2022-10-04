6849a1a6a76f995879e46ed8c94d1ae99d788466
milimolo <bondemand@hotmail.dk>
2022-10-04 12:47:45 +0200

    Initial commit

---
+++ PrimeGenerator/PrimeGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeGenerator
+{
+    public class PrimeGenerator
+    {
+
+        List<long> GetPrimesSequential(long first, long last)
+        {
+            List<long> primes = new List<long>();
+
+            return primes;
+        }
+
+        List<long> GetPrimesParallel(long first, long last)
+        {
+            List<long> primes = new List<long>();
+
+            return primes;
+        }
+
+    }
+}
