fa3ff7bd0ebc31f249918bdc3121bd9afba27bf5
milimolo <bondemand@hotmail.dk>
2022-10-04 15:30:25 +0200

    Opgave 1 og 2 done

--- PrimeGenerator/PrimeGenerator.cs
+++ PrimeGenerator/PrimeGenerator.cs
@@ -1,27 +1,61 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PrimeGenerator
+namespace PrimeGeneratorApp
 {
     public class PrimeGenerator
     {
 
-        List<long> GetPrimesSequential(long first, long last)
+        public List<long> GetPrimesSequential(long first, long last)
         {
-            List<long> primes = new List<long>();
+            var numbers = Enumerable.Range((int)first, (int)(last))
+                .Where(number => IsPrime(number) && number <= last)
+                .ToList();
+
+            List<long> primes = numbers.Select(i => (long)i).ToList();
+            primes.Sort();
 
             return primes;
         }
 
-        List<long> GetPrimesParallel(long first, long last)
+        public List<long> GetPrimesParallel(long first, long last)
         {
-            List<long> primes = new List<long>();
+            var numbers = Enumerable.Range((int)first, (int)(last)).ToList();
 
+            var primeNumbers = new ConcurrentBag<long>();
+
+            Parallel.ForEach(numbers, number =>
+            {
+                if (IsPrime(number) && number <= last)
+                {
+                    primeNumbers.Add(number);
+                }
+            });
+
+            List<long> primes = primeNumbers.Select(i => (long)i).ToList();
+            primes.Sort();
             return primes;
         }
 
+        private static bool IsPrime(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+
+            for (var divisor = 2; divisor <= Math.Sqrt(number); divisor++)
+            {
+                if (number % divisor == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
--- PrimeGenerator/Program.cs
+++ PrimeGenerator/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using PrimeGeneratorApp;
+using System.Diagnostics;
+
+Console.WriteLine("Prime numbers in prime time!");
+
+PrimeGenerator pg = new PrimeGenerator();
+var swSequential = Stopwatch.StartNew();
+List<long> PrimesSequential = pg.GetPrimesSequential(1, 1000000);
+swSequential.Stop();
+
+var swParallel = Stopwatch.StartNew();
+List<long> PrimesParallel = pg.GetPrimesParallel(1, 1000000);
+swParallel.Stop();
+
+Console.WriteLine($"Classical foreach loop | Total prime numbers : {PrimesSequential.Count} | Time Taken : {swSequential.ElapsedMilliseconds} ms.");
+Console.WriteLine($"Parallel.ForEach loop  | Total prime numbers : {PrimesParallel.Count} | Time Taken : {swParallel.ElapsedMilliseconds} ms.");
+
+
+//foreach (var n in PrimesParallel)
+//{
+//    Console.WriteLine(n);
+//}
+Console.ReadLine();
+
