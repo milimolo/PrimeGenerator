2fcba24178a6a541b54239c830b1369d86f6a5ec
milimolo <bondemand@hotmail.dk>
2022-10-04 15:57:23 +0200

    Opgave 3

--- PrimeGenerator/Program.cs
+++ PrimeGenerator/Program.cs
@@ -1,25 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-using PrimeGeneratorApp;
+﻿using PrimeGeneratorApp;
 using System.Diagnostics;
 
 Console.WriteLine("Prime numbers in prime time!");
 
 PrimeGenerator pg = new PrimeGenerator();
 var swSequential = Stopwatch.StartNew();
-List<long> PrimesSequential = pg.GetPrimesSequential(1, 1000000);
+pg.GetPrimesSequential(1, 1000000);
 swSequential.Stop();
 
 var swParallel = Stopwatch.StartNew();
-List<long> PrimesParallel = pg.GetPrimesParallel(1, 1000000);
+pg.GetPrimesParallel(1, 1000000);
 swParallel.Stop();
 
-Console.WriteLine($"Classical foreach loop | Total prime numbers : {PrimesSequential.Count} | Time Taken : {swSequential.ElapsedMilliseconds} ms.");
-Console.WriteLine($"Parallel.ForEach loop  | Total prime numbers : {PrimesParallel.Count} | Time Taken : {swParallel.ElapsedMilliseconds} ms.");
+//-----------------------------------------------
 
+var swSeq1To10Mil = Stopwatch.StartNew();
+pg.GetPrimesSequential(1, 10000000);
+swSeq1To10Mil.Stop();
 
-//foreach (var n in PrimesParallel)
-//{
-//    Console.WriteLine(n);
-//}
+var swPara1To10Mil = Stopwatch.StartNew();
+pg.GetPrimesParallel(1, 10000000);
+swPara1To10Mil.Stop();
+
+//-----------------------------------------------
+
+var swSeq1MilTo2Mil = Stopwatch.StartNew();
+pg.GetPrimesSequential(1, 10000000);
+swSeq1MilTo2Mil.Stop();
+
+var swPara1MilTo2Mil = Stopwatch.StartNew();
+pg.GetPrimesParallel(1, 10000000);
+swPara1MilTo2Mil.Stop();
+
+//-----------------------------------------------
+
+var swSeq10MilTo20Mil = Stopwatch.StartNew();
+pg.GetPrimesSequential(1, 10000000);
+swSeq10MilTo20Mil.Stop();
+
+var swPara10MilTo20Mil = Stopwatch.StartNew();
+pg.GetPrimesParallel(1, 10000000);
+swPara10MilTo20Mil.Stop();
+
+// a
+Console.WriteLine($"Sequential foreach loop | 1 - 1.000.000 | Time Taken : {swSequential.ElapsedMilliseconds} ms.");
+Console.WriteLine($"Parallel.ForEach loop  | 1 - 1.000.000 | Time Taken : {swParallel.ElapsedMilliseconds} ms.\n");
+
+// b
+Console.WriteLine($"Sequential foreach loop | 1 - 10.000.000 | Time Taken : {swSeq1To10Mil.ElapsedMilliseconds} ms.");
+Console.WriteLine($"Parallel.ForEach loop  | 1 - 10.000.000 | Time Taken : {swPara1To10Mil.ElapsedMilliseconds} ms.\n");
+
+// c
+Console.WriteLine($"Sequential foreach loop | 1.000.000 - 2.000.000 | Time Taken : {swSeq1MilTo2Mil.ElapsedMilliseconds} ms.");
+Console.WriteLine($"Parallel.ForEach loop  | 1.000.000 - 2.000.000 | Time Taken : {swPara1MilTo2Mil.ElapsedMilliseconds} ms.\n");
+
+// d
+Console.WriteLine($"Sequential foreach loop | 10.000.000 - 20.000.000 | Time Taken : {swSeq10MilTo20Mil.ElapsedMilliseconds} ms.");
+Console.WriteLine($"Parallel.ForEach loop  | 10.000.000 - 20.000.000 | Time Taken : {swPara10MilTo20Mil.ElapsedMilliseconds} ms.");
+
 Console.ReadLine();
 
